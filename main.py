8a95c7d7de01e816b36ec8b94a533aadae085b0c
Виталий Суслов <arthas.menethil2002@bk.ru>
2022-04-14 14:30:10 +0700

    Add table for F*(x) stepped graph

--- main.py
+++ main.py
@@ -51,24 +51,63 @@ def interval_row(data):
     m /= sum(n_i)
     d = d / sum(n_i) - (m * m)
     sigma = math.sqrt(d)
-    numerical_estimates = (['m', 'D', 'Sigma', 'h'], [m, d, sigma, h])
+    numerical_estimates = (['M', 'D', 'Sigma', 'h'], [m, d, sigma, h])
+
+    # функция F*(x) (информация для построения графика в function_graph_x/y)
+    function = []
+    function_graph_x = ['x    ', 0]
+    function_graph_y = ['F*(x)', 0]
+    current_value = 0
+    condition = str(x[0])
+    for i in range(0, len(n_i)):
+        temp_value = current_value + n_i[i] / sum(n_i)
+
+        if temp_value != current_value:
+            if current_value == 0:
+                condition = 'x <= ' + str(x[i])
+                # function_graph_x.append(x[i])
+            else:
+                condition += ' < x <= ' + str(x[i])
+            function.append([current_value, condition])
+
+            function_graph_x.append(str(x[i]).replace('.', ','))
+            function_graph_y.append(str(temp_value).replace('.', ','))
+
+            current_value = temp_value
+            condition = str(x[i])
+
+    current_value = 1
+    condition = 'x > ' + str(x[len(x) - 1])
+    function.append([current_value, condition])
+
+    function_graph_x.append(str(x[i] + 1).replace('.', ','))
+    function_graph_y.append(1)
 
     # запись таблицы в csv файл
     with open('table.csv', mode='w', encoding='utf-8') as file:
-        out = csv.writer(file)
+        out = csv.writer(file, delimiter='\t')
         out.writerow(['interval number', 'interval', 'ni', 'w', 'xi', 'xi*ni', 'xi*xi*ni'])
         for i in range(len(intervals)):
             out.writerow([interval_indexes[i], intervals[i], n_i[i], w[i], x[i], x_i_n_i[i], x2_i_n_i[i]])
+        out.writerow(['Numerical estimates'])
         out.writerow(numerical_estimates[0])
         out.writerow(numerical_estimates[1])
+        out.writerow(['Function F*(x)'])
+        out.writerow(['F*(x) value', 'Condition'])
+        for row in function:
+            out.writerow(row)
+        out.writerow(['Function F*(x) graph'])
+        out.writerow(function_graph_x)
+        out.writerow(function_graph_y)
 
 
 if __name__ == '__main__':
     # test data from textbook
     # data1 = [11, 8, 12, 5, 3, 10, 7, 8, 3, 6, 31, 9, 5, 7, 13, 4, 4, 0, 3, 6]
     # data2 = [19, 20, 19, 19, 20, 20, 18, 18, 15, 19, 20, 17, 23, 23, 21, 24, 20, 22, 20, 23, 20, 23, 21, 19, 21, 20,
-    #          19, 21, 17, 20]
+    #         19, 21, 17, 20]
     # data3 = [2, 2, 2, 2, 2, 1, 1, 0, 3, 1, 1, 1, 5, 4, 4, 4, 0, 1, 1, 4, 1, 0, 9, 1, 4, 5, 2, 1, 2, 1]
+    # data4 = [1, 2, 1, 2, 2, 1, 1, 3, 4, 1, 1, 2, 3, 3, 1]
     print('Enter sample data without commas.')
     data_input = [int(s) for s in input().split()]
     interval_row(data_input)
